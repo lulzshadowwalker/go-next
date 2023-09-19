eb11b288b7036b561ab0ffd557149a25a29aa0e3
lulzshadowwalker <lulzshadowwalker@gmail.com>
2023-09-19 09:44:26 +0300

    web revamp init

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,19 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import { Cinzel } from 'next/font/google'
-
-const cinzel = Cinzel({ subsets: ['latin'] })
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <style jsx global>{`
-        html {
-          font-family: ${cinzel.style.fontFamily};
-        }
-      `}</style>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
